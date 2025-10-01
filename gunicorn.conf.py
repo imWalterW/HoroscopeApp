a9fa7148c6659a90501bdbd19e4013460ad20bad
imWalterW <imhisenberg@gmail.com>
2025-10-01 15:07:47 +0530

    Bug fix

---
+++ gunicorn.conf.py
@@ -0,0 +1,17 @@
+import multiprocessing
+
+# Server socket
+# Render will automatically set the PORT environment variable
+bind = "0.0.0.0:10000" 
+
+# Worker processes
+# This will be (2 * number of CPU cores) + 1
+workers = (multiprocessing.cpu_count() * 2) + 1
+worker_class = "uvicorn.workers.UvicornWorker"
+
+# Set a long timeout to allow for Gemini API calls
+timeout = 120
+
+# Logging
+accesslog = "-"
+errorlog = "-"
